4953b1a46ef445d685c45d33cdc41b7606bf9dd4
red-shinku <3480198982@qq.com>
2025-09-15 22:55:01 +0800

    更新slice()，Counter()，namedtuple()

--- py/数据结构与算法/try.py
+++ py/数据结构与算法/try.py
@@ -1,3 +1,35 @@
+#---------------------------------------------------------------
+
+from collections import Counter # 序列中元素出现的次数
+
+names = ["shinku", "shinku", "shinku", "kotori", "kotori", "kotori", "kotori", "komari", "komari", 
+        "setsumi", "setsumi", "shizuru", "lucia", "chihaya", "taichi", "kutarou"]
+
+name_counts = Counter(names)
+print(f'名字频率最高 { name_counts.most_common(3) }', end= '\n')
+# Counter()返回的对象可以做数学运算
+
+#---------------------------------------------------------------
+
+ # 切片与其重命名
+items = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+print(f'使用切片[2:4] { items[2: 4] }', end='\n')
+a = slice(2, 4)
+print(f'使用切片名a { items[a] }', end='\n')
+
+#---------------------------------------------------------------
+
+from collections import namedtuple # 为元组内每个元素命名
+
+# 该函数返回具有命名字段的元组子类对象
+Student = namedtuple('Student', ['name', 'clas', 'id'])
+stu = Student('Maek', 5, 529)
+print(f'{ stu.name } 他在 { stu.clas } 班', end='\n')
+stu = [ Student('romeo', 3, 342), Student('julia', 3, 341)]
+
+
+#---------------------------------------------------------------
+
 # 从任意长度的可迭代对象中分解元素
 # *表达式，这里提取出中间的元素们
 def drop_first_last(grades):
